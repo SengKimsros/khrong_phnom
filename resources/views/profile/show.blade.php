a13bc0774fb34bce701f123619e7618e8e7946c0
SengKimsros <80749347+SengKimsros@users.noreply.github.com>
2021-04-02 16:41:14 +0700

    Jetstream

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -4,16 +4,17 @@
             {{ __('Profile') }}
         </h2>
     </x-slot>
-
     <div>
         <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
             @if (Laravel\Fortify\Features::canUpdateProfileInformation())
+
                 @livewire('profile.update-profile-information-form')
 
                 <x-jet-section-border />
             @endif
 
             @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updatePasswords()))
+
                 <div class="mt-10 sm:mt-0">
                     @livewire('profile.update-password-form')
                 </div>
