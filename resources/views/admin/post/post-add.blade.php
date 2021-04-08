64268d58216d9c86d9cbd26a111da232e8f62e67
SengKimsros <80749347+SengKimsros@users.noreply.github.com>
2021-04-08 08:55:07 +0700

    Post

--- resources/views/admin/post/post-add.blade.php
+++ resources/views/admin/post/post-add.blade.php
@@ -1,11 +1,65 @@
-@extends('../layouts.app')
+@extends('layouts.app')
 @section('content')
 <div class="page-wrapper">
     <!-- ===== Page-Container ===== -->
     <div class="container-fluid">
-        <div class="page-wrapper" style="min-height: 525px;">
-            <div class="container-fluid">
-                <h1>Add Post</h1>
+        <div class="" style="min-height: 525px;">
+            {{-- @dump($user) --}}
+            <div class="">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="white-box">
+                            <h3 class="box-title m-b-0">Add Post</h3><br><br>
+                            <div class="row">
+                                @if (count($errors) > 0)
+                                    <div class="alert alert-danger">
+                                        <strong>Whoops!</strong> There were some problems with your input.
+                                        <ul>
+                                            @foreach ($errors->all() as $error)
+                                                <li>{{ $error }}</li>
+                                            @endforeach
+                                        </ul>
+                                    </div>
+                                @endif
+                                <form method="POST" action="/admin/user/{{$user['id'] ?? null}}" enctype="multipart/form-data">
+                                    @if (isset($user))
+                                    <input name="_method" type="hidden" value="PUT">
+                                    @endif
+                                    @csrf
+                                    <div class="col-sm-12 col-xs-12">
+                                        <div class="form-group">
+                                            <label for="">Title</label>
+                                            <span class="text-danger">*</span>
+                                            <input type="text" name="first_name" class="form-control required_name" value="{{$user['first_name'] ?? ''}}">
+                                        </div>
+                                    </div>
+
+
+                                    <div class="col-sm-12 col-xs-12">
+                                        <div class="form-group">
+                                            <label for="">Content</label>
+                                            <span class="text-danger">*</span>
+                                            <textarea name="content" id="content"></textarea>
+                                        </div>
+
+                                    </div>
+
+                                    <div class="col-sm-12 col-xs-12">
+                                        <div class="form-group">
+                                            <label for="">Content</label>
+                                            <span class="text-danger">*</span>
+                                            <input type="file" class="form-control" name="thumnail">
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-12 col-xs-12 text-right">
+                                        <a href="{{url('admin/user')}}" class="btn btn-danger">Cancel</a>
+                                        <input type="submit" class="btn btn-info" value="Submit">
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -14,4 +68,21 @@
         © 2017 Cubic Admin
     </footer>
 </div>
+@endsection
+
+@section("customScript")
+    <script src="/ckeditor/ckeditor.js"></script>
+    <script src="/vendor/laravel-filemanager/js/stand-alone-button.js"></script>
+    <script>
+
+        var options = {
+            filebrowserImageBrowseUrl: '/laravel-filemanager?type=Images',
+            filebrowserImageUploadUrl: '/laravel-filemanager/upload?type=Images&_token=',
+            filebrowserBrowseUrl: '/laravel-filemanager?type=Files',
+            filebrowserUploadUrl: '/laravel-filemanager/upload?type=Files&_token='
+        };
+        CKEDITOR.replace('content',options);
+        var route_prefix1 = "/laravel-filemanager?type=Files";
+        $('#Plan').filemanager('file', {prefix: route_prefix1});
+    </script>
 @endsection
--- resources/views/admin/post/post.blade.php
+++ resources/views/admin/post/post.blade.php
@@ -1,17 +1,101 @@
 @extends('../layouts.app')
 @section('content')
+<!-- Modal Delete User -->
+<div class="modal fade" id="modal_delete_user" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLongTitle">Delete Post  !!!</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <form action="" method="POST" id="fm_delete_user">
+            @csrf
+            <input name="_method" type="hidden" value="DELETE">
+        <div class="modal-body">
+
+            <Strong>Are you sure ?</Strong>
+
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
+            <button type="submit" class="btn btn-info">Delete</button>
+        </div>
+    </form>
+      </div>
+    </div>
+  </div>
+  {{-- end Modal Delete User --}}
 <div class="page-wrapper">
     <!-- ===== Page-Container ===== -->
     <div class="container-fluid">
-        <div class="page-wrapper" style="min-height: 525px;">
-            <div class="container-fluid">
-                <h1>Post</h1>
+        <div class="row">
+            <div class="col-md-12" style="text-align: right;">
+                <div class="form-group">
+                    @if (RolePermission(4,1))
+                        <a href="{{url('admin/user/create')}}" class="btn btn-success" tabindex="0" type="button">
+                            <span data-action="create"><i class="fa fa-plus"></i>{{__('Create New')}}</span>
+                        </a>
+                    @endif
+                </div>
             </div>
         </div>
+        <div style="min-height: 525px;">
+            <div class="white-box">
+                <table id="btnProject" class="table table-large mb-0  nowrap w-100">
+                    <thead>
+                        <tr>
+                            <th>Image</th>
+                            <th>Title</th>
+                            <th>Content</th>
+                            <th>Author</th>
+                            <th>Status</th>
+
+                            <th style="text-align: center;">{{__('Actions')}}</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {{-- @foreach (($users ?? null) as $item)
+                                <tr>
+                                    <th><img src="/{{$item->profile_photo_path ?? ''}}" alt="" style="width: 60px;height: 60px;object-fit: cover;"></th>
+                                    <th style="padding-top: 30px;"> <a href="javascript:">{{$item->username ?? ''}}</a> </th>
+                                    <td>
+                                        @if (($item->gender ?? 0)==1)
+                                            Male
+                                        @else
+                                            Female
+                                        @endif
+                                    </td>
+                                    <th style="padding-top: 30px;">{{$item->phone}}</th>
+                                    <td class="align-middle text-center">{{$item->email ?? ''}}</td>
+                                    <td>{{$item->position_name ?? ''}}</td>
+                                    <th style="padding-top: 30px;text-align: center;">
+                                        @if (RolePermission(4,2))
+                                            <a href="{{url('admin/user')}}/{{$item->id ?? ''}}/edit" id="btnEdit" data-id="{{$item->id}}" class="btn btn-primary btn-sm">{{__('Edit')}}</a>
+                                        @endif
+                                        @if (RolePermission(4,3))
+                                            <a href="javascript:" class="btn btn-danger btn-sm" onclick="delete_user({{$item->id ?? 0}})">{{__('Delete')}}</a>
+                                        @endif
+                                    </th>
+                                </tr>
+                            @endforeach --}}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
     </div>
     <!-- ===== Page-Container-End ===== -->
     <footer class="footer t-a-c">
         © 2017 Cubic Admin
     </footer>
 </div>
+<script>
+    function delete_user(id){
+        $('#modal_delete_user').modal('show');
+        $('#fm_delete_user').attr('action', '/admin/user/'+id);
+    }
+</script>
 @endsection
